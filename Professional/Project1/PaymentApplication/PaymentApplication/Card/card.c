7a5e9f64ef16eaaf50e1235612d1660c43f1a83c
Mohanad Magdy <114406337+MohanadAtlam@users.noreply.github.com>
2022-11-16 19:22:04 +0200

    Adjusting

--- Professional/Project1/PaymentApplication/PaymentApplication/Card/card.c
+++ Professional/Project1/PaymentApplication/PaymentApplication/Card/card.c
@@ -6,6 +6,8 @@
 #include "STD_TYPES_FWD.h"
 #include "card.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 EN_cardError_t getCardHolderName(ST_cardData_t* cardData)
 {
@@ -15,6 +17,12 @@ EN_cardError_t getCardHolderName(ST_cardData_t* cardData)
 	//while loop to get the string length     ==== same to all funcitons in this module
 	while (cardData->cardHolderName[counter] != 0)
 	{
+		//cheching if not from a->z , A->Z and not a space
+		if ((cardData->cardHolderName[counter] < 'a' || cardData->cardHolderName[counter]>'z') && (cardData->cardHolderName[counter] < 'A' || cardData->cardHolderName[counter]>'Z') && cardData->cardHolderName[counter] != ' ')
+		{
+			printf("--> Name you enter must contain only characters\n");
+			return WRONG_NAME;
+		}
 		counter++;
 	}
 	//check if the name is less than 20 elements or its 0 or more than 24 elements  
@@ -36,8 +44,26 @@ EN_cardError_t getCardExpiryDate(ST_cardData_t* cardData)
 	uint8_t counter = 0;
 	while (cardData->cardExpirationDate[counter] != 0)
 	{
+		//cheching if not a number and not a /
+		if ((cardData->cardExpirationDate[counter]<'0' || cardData->cardExpirationDate[counter]>'9')&& cardData->cardExpirationDate[counter]!='/')
+		{
+			printf("--> date you enter must contain only numbers and be in the form of MM/YY\n");
+			return WRONG_EXP_DATE;
+		}
 		counter++;
 	}
+	if (cardData->cardExpirationDate[2] != '/')
+	{
+		printf("--> Wrong date form entered\n");
+		return WRONG_EXP_DATE;
+	}
+
+	if (atoi(cardData->cardExpirationDate) < 0 || atoi(cardData->cardExpirationDate) > 12)
+	{
+		printf("--> Wrong date entered\n");
+		return WRONG_EXP_DATE;
+	}
+
 	if (counter != 5 || cardData->cardExpirationDate[0] == 0)
 	{
 		printf("\nWrong EXP Date\n");
@@ -58,6 +84,11 @@ EN_cardError_t getCardPAN(ST_cardData_t* cardData)
 	uint8_t counter = 0;
 	while (cardData->primaryAccountNumber[counter] != 0)
 	{
+		if (cardData->cardExpirationDate[counter] < '0' || cardData->cardExpirationDate[counter]>'9')
+		{
+			printf("--> PAN you enter must contain only numbers\n");
+			return WRONG_PAN;
+		}
 		counter++;
 	}
 	if (counter <16 || cardData->primaryAccountNumber[0] == 0 || counter >19)
